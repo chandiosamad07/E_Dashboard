cf7e576b1a9b094e886a7e9c89d35865f11a0c3f
chandiosamad07 <absamadchandio@gmamil.com>
2024-07-11 13:04:14 +0500

    Routing add in Reactjs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Nav from './Nav';
+import { BrowserRouter,Route ,Routes} from 'react-router-dom';
 function App() {
   return (
     <>
-    <h1>E-Dashboard</h1>
+    <BrowserRouter>
+    <Nav/>
+    <Routes>
+      <Route path='/'element={<h1>Product list Component</h1>}></Route>
+      <Route path='/add'element={<h1>Add Product  Component</h1>}></Route>
+      <Route path='/update'element={<h1>Update Product Component</h1>}></Route>
+      <Route path='/logout'element={<h1>logout Component</h1>}></Route>
+      <Route path='/profile'element={<h1>Profile Product list Component</h1>}></Route>
+    </Routes>
+    </BrowserRouter>
     </>
   );
 }

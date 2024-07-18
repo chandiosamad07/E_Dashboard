424e0d0a3d2d45a6c2da721204cbbb10903c6e0a
chandiosamad07 <absamadchandio@gmamil.com>
2024-07-18 12:49:57 +0500

    Checking user Signup for routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,18 +5,21 @@ import Footer from './Components/Footer';
 import Nav from './Components/Nav';
 import { BrowserRouter,Route ,Routes} from 'react-router-dom';
 import Signup from './Components/Signup';
-
+import PrivateComponent from './Components/PrivateComponent';
 function App() {
   return (
     <>
     <BrowserRouter>
     <Nav/>
     <Routes>
+
+      <Route element = {<PrivateComponent/>}>
       <Route path='/'element={<h1>Product list Component</h1>}></Route>
       <Route path='/add'element={<h1>Add Product  Component</h1>}></Route>
       <Route path='/update'element={<h1>Update Product Component</h1>}></Route>
       <Route path='/logout'element={<h1>logout Component</h1>}></Route>
       <Route path='/profile'element={<h1>Profile Product list Component</h1>}></Route>
+    </Route>
       <Route path='/signup'element={<Signup/>} />
     </Routes>
     <Footer></Footer>

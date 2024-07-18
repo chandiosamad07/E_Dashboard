952f2b01f4085ef9aadb7dff57709ade856e3317
chandiosamad07 <absamadchandio@gmamil.com>
2024-07-18 10:13:21 +0500

    Signup API

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app =  express()
+require('./DB/config')
+const User = require('./DB/User');
 
+app.use(express.json())
+
+app.post('/register',async (req,res)=>{
+    let user = new User(req.body)
+    let result = await user.save();
+    res.send(result)
+
+})
 
 app.get('/',(req,res)=>{
     res.send("Backend is working")
